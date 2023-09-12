2ebe2534039ffe707a3b35deeb5fba5d8dbe23eb
jc <root@sha3.dev>
2023-09-12 11:42:15 +0200

    fix

--- src/lib/lemon-squeezy.ts
+++ src/lib/lemon-squeezy.ts
@@ -43,22 +43,61 @@ export default class LemonSqueezy {
   private async sendRequest(partialUrl: string) {
     const lemonSqueezyUrl = `${LEMON_SQUEEZY_API_BASE_URL}${partialUrl}`;
     logger.debug(`lemon squeezy request: ${lemonSqueezyUrl}`);
-    const response = await fetch(lemonSqueezyUrl, { headers: this.getHeaders() });
-    if (response.ok) {
-      const json = await response.json();
-      return json;
+    try {
+      const response = await fetch(lemonSqueezyUrl, { headers: this.getHeaders() });
+      if (response.ok) {
+        const body = await response.json();
+        return { body, status: response.status };
+      }
+      return { error: true, status: response.status };
+    } catch (e: any) {
+      return { error: true, status: 500, message: e?.cause?.message || e.message };
     }
-    throw new Error(`invalid lemon squeezy response: ${response.status}`);
   }
 
   /**
    * public : methods
    */
 
-  public async getCustomer(email: string) {
-    const json = await this.sendRequest(
-      `https://api.lemonsqueezy.com/v1/customers?filter[store_id]=42298&filter[email]=${encodeURIComponent(email)}`
-    );
-    return json;
+  public async getCustomerByEmail(email: string) {
+    const { storeId } = this.options;
+    const encodedEmail = encodeURIComponent(email);
+    const partialUrl = `/v1/customers?filter[store_id]=${storeId}=&filter[email]=${encodedEmail}`;
+    const result = await this.sendRequest(partialUrl);
+    if (result.status === 404) {
+      return null;
+    }
+    if (result.status !== 200) {
+      const error = result.body?.errors?.[0];
+      throw new Error(error || `error retrieving customer`);
+    }
+    const data = result?.body?.data?.[0];
+    if (data) {
+      return {
+        id: result?.body?.data?.[0]?.id,
+        ...result?.body?.data?.[0]?.attributes
+      };
+    }
+    throw new Error(`invalid customer response`);
+  }
+
+  public async getCustomerById(id: string) {
+    const partialUrl = `/v1/customers/${id}`;
+    const result = await this.sendRequest(partialUrl);
+    if (result.status === 404) {
+      return null;
+    }
+    if (result.status !== 200) {
+      const error = result.body?.errors?.[0];
+      throw new Error(error || `error retrieving customer`);
+    }
+    const data = result?.body?.data?.[0];
+    if (data) {
+      return {
+        id: result?.body?.data?.id,
+        ...result?.body?.data?.attributes
+      };
+    }
+    throw new Error(`invalid customer response`);
   }
 }
